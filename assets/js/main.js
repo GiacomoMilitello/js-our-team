2d781b97f4f98dbcd23633fd6ee85c9b40e17e6a
GiacomoMilitello <g.militello95@gmail.com>
2024-01-19 15:23:31 +0100

    img problem

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,3 +12,55 @@ Trasformare la stringa foto in una immagine effettiva
 BONUS 2:
 Organizzare i singoli membri in card/schede
 */
+
+const team = [
+    {
+    name: "Wayne Barnett",
+    role: "Founder & CEO",
+    photo: "../img/wayne-barnett-founder-ceo.jpg"
+},
+    {
+    name: "Angela Caroll",
+    role: "Chief Editor",
+    photo: "../img/angela-caroll-chief-editor.jpg"
+},
+    {
+    name: "Walter Gordon",
+    role: "Office Manager",
+    photo: "../img/walter-gordon-office-manager.jpg"
+},
+    {
+    name: "Angela Lopez",
+    role: "Social Media Manager",
+    photo: "../img/angela-lopez-social-media-manager.jpg"
+},
+    {
+    name: "Scott Estrada",
+    role: "Developer",
+    photo: "../img/scott-estrada-developer.jpg"
+},
+    {
+    name: "Barbara Ramos",
+    role: "Graphic Designer",
+    photo: "../img/barbara-ramos-graphic-designer.jpg"
+}
+];
+
+console.log( team );
+
+for(let i = 0; i < team.length; i++) {
+    let member = team[i];
+    document.querySelector("main .row").innerHTML += `
+         <div class="card col">
+                 <img class="card-img-top" src='${member.photo}' alt="${member.name}" />
+                 <div class="card-body">
+                     <h4 class="card-title">
+                     ${member.name}
+                     </h4>
+                     <p class="card-text">
+                     ${member.role}
+                     </p>
+                 </div>
+             </div>
+    `
+}
